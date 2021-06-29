8545217e257cf9699d47b6c9b529981436e03d2c
Gabrie345 <gabriel345wp@gmail.com>
2021-06-29 17:27:55 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    const cartas = [
+        "imagens/angular.png",
+        "imagens/css3.png",
+        "imagens/html5.png",
+        "imagens/nodejs.png"
+    ]
+
+
+
+
+
+
+
+    $(".botao").click(function () {
+
+    })
+
+})
