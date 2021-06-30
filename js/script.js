273f5604553f55ba54f0478eeb47cee0a68b38e3
Gabrie345 <gabriel345wp@gmail.com>
2021-06-30 13:07:59 -0300

    Codigo Funcionando e exibindo informaçoes no console

--- js/script.js
+++ js/script.js
@@ -52,26 +52,63 @@ $(document).ready(function () {
                 src: "imagens/nodejs.png",
             }))
     ]
-
+    var Pontos = 0;
+    const areaDoJogo = $(".areaDoJogo");
+    var HistoricoDeClick = 0;
+    var primeiroclick = 0;
+    var Segundoclick = 0;
+    var imagemUm = "";
+    var imagemDois = "";
 
     //função para resetar 
     imagens = imagens.sort(() => Math.random() - 0.5)
-    console.log([imagens])
-    const areaDoJogo = $(".areaDoJogo")
     imagens.forEach(imagens => {
-
         (imagens).appendTo($("#areaDoJogo"))
-
     })
-
+    function carregaJogo() {
+        imagens = imagens.sort(() => Math.random() - 0.5)
+        imagens.forEach(imagens => {
+            (imagens).appendTo($("#areaDoJogo"))
+        })
+    }
+    //Recebe os valores das cartas
     $(".carta").click(function () {
-        console.log($(this).val())
+
+        if (primeiroclick == 0) {
+            primeiroclick = ($(this).val());
+            HistoricoDeClick += 1;
+            imagemUm = $(this);
+
+        } else if (Segundoclick == 0) {
+            Segundoclick = ($(this).val());
+            HistoricoDeClick += 1;
+            imagemDois = $(this);
+
+            verificadorAcertosErros();
+        }
     })
 
+    //verifica se acertou ou erro
+    function verificadorAcertosErros() {
+        if (primeiroclick == Segundoclick) {
+            console.log("acertou");
+            Pontos += 2;
 
+            imagemUm.remove();
+            imagemDois.remove();
+
+            primeiroclick = 0;
+            Segundoclick = 0;
+
+        } else if (primeiroclick != Segundoclick) {
+            console.log("errou")
+            primeiroclick = 0
+            Segundoclick = 0
+        }
+    }
 
     $(".botao").click(function () {
-
+        carregaJogo()
 
     })
 
