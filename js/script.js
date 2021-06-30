6511a791dea50258f14a4cf3e66e91da666aafd2
Gabrie345 <gabriel345wp@gmail.com>
2021-06-30 10:35:19 -0300

    Criação do Javascript

--- js/script.js
+++ js/script.js
@@ -1,18 +1,77 @@
 $(document).ready(function () {
-    const cartas = [
-        "imagens/angular.png",
-        "imagens/css3.png",
-        "imagens/html5.png",
-        "imagens/nodejs.png"
+
+    var imagens = [
+        img1 = ($('<img />',
+            {
+                class: "carta",
+                val: "1",
+                src: "imagens/angularjs.png",
+            })),
+
+        img2 = ($('<img />',
+            {
+                class: "carta",
+                val: "2",
+                src: "imagens/css3.png",
+            })),
+        img3 = ($('<img />',
+            {
+                class: "carta",
+                val: "3",
+                src: "imagens/html5.png",
+            })),
+        img3 = ($('<img />',
+            {
+                class: "carta",
+                val: "4",
+                src: "imagens/nodejs.png",
+            })),
+        img1 = ($('<img />',
+            {
+                class: "carta",
+                val: 1,
+                src: "imagens/angularjs.png",
+            })),
+
+        img2 = ($('<img />',
+            {
+                class: "carta",
+                val: 2,
+                src: "imagens/css3.png",
+            })),
+        img3 = ($('<img />',
+            {
+                class: "carta",
+                val: 3,
+                src: "imagens/html5.png",
+            })),
+        img3 = ($('<img />',
+            {
+                class: "carta",
+                val: 4,
+                src: "imagens/nodejs.png",
+            }))
     ]
 
 
+    //função para resetar 
+    imagens = imagens.sort(() => Math.random() - 0.5)
+    console.log([imagens])
+    const areaDoJogo = $(".areaDoJogo")
+    imagens.forEach(imagens => {
 
+        (imagens).appendTo($("#areaDoJogo"))
 
+    })
+
+    $(".carta").click(function () {
+        console.log($(this).val())
+    })
 
 
 
     $(".botao").click(function () {
+
 
     })
 
